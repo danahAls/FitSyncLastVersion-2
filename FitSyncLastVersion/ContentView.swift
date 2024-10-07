0fb34dceea181d99169c81a1aaa10e3988225968
Nouraibn <Noura_alnassar@hotmail.com>
2024-10-07 10:06:21 +0300

    Initial Commit

---
+++ FitSyncLastVersion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FitSyncLastVersion
+//
+//  Created by Noura on 04/04/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FitSyncLastVersion/FitSyncLastVersionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitSyncLastVersionApp.swift
+//  FitSyncLastVersion
+//
+//  Created by Noura on 04/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct FitSyncLastVersionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
